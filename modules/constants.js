9a29719d37d40582b091d9f9f7ee4792dc538bb3
Ivan Honcharenko <gonivanko@gmail.com>
2024-11-21 19:28:49 +0200

    Add Ukrainian and Russian keys support

--- modules/constants.js
+++ modules/constants.js
@@ -9,3 +9,8 @@ export const yRotate = document.getElementById("y-rotate");
 export const sideX = document.getElementById("side-x");
 export const sideY = document.getElementById("side-y");
 export const height = document.getElementById("height");
+
+export const movementUpKeys = ["w", "ц"];
+export const movementDownKeys = ["s", "і", "ы"];
+export const movementLeftKeys = ["a", "ф"];
+export const movementRightKeys = ["d", "в"];
--- modules/keyboard.js
+++ modules/keyboard.js
@@ -1,4 +1,4 @@
-import { xMove, yMove } from "./constants.js";
+import { xMove, yMove, movementDownKeys, movementUpKeys, movementLeftKeys, movementRightKeys } from "./constants.js";
 import { draw } from "./draw.js";
 let intervalId;
 const keysPressed = new Set(); 
@@ -6,53 +6,61 @@ let movementX = 0;
 let movementY = 0;
 const movementValue = 2;
 
+function setHasValues(set, valuesArray) {
+    return valuesArray.some(value => set.has(value));
+}
+
+function deleteSetValues(set, valuesArray) {
+    valuesArray.forEach(value => set.delete(value));
+}
+
 export function handleKeyDown(event) {
     keysPressed.add(event.key);
-    
-    // }
-    if (keysPressed.has("w")) {
+
+    if (setHasValues(keysPressed, movementUpKeys)) {
         movementY = -movementValue;
     }
-    else if (keysPressed.has("s")) {
+    else if (setHasValues(keysPressed, movementDownKeys)) {
         movementY = movementValue;
     }
 
-    if (keysPressed.has("a")) {
+    if (setHasValues(keysPressed, movementLeftKeys)) {
         movementX = -movementValue;
     }
-    else if (keysPressed.has("d")) {
+    else if (setHasValues(keysPressed, movementRightKeys)) {
         movementX = movementValue;
     }
     if (!intervalId) { 
         intervalId = setInterval(() => {
             xMove.value = parseInt(xMove.value || "0", 10) + movementX;
             yMove.value = parseInt(yMove.value || "0", 10) + movementY;
-            draw(); // Redraw the cube
-            // console.log("xMove.value, yMove.value:", xMove.value, yMove.value);
-        }, 16); // Increment every 100ms
+            draw();
+        }, 16);
     }
 }
 
 export function handleKeyUp(event) {
-    if (event.key === "w" || event.key === "a" || event.key === "s" || event.key === "d") {
+
+    let verticalKeys = movementDownKeys.concat(movementUpKeys);
+    let horizontalKeys = movementLeftKeys.concat(movementRightKeys);
+    let movementKeys = movementUpKeys.concat(movementDownKeys).concat(movementLeftKeys).concat(movementRightKeys);
+
+    if (movementKeys.includes(event.key)) {
         stopInterval(intervalId);
     }
-    if (event.key === "w" || event.key === "s") {
-        keysPressed.delete("w");
-        keysPressed.delete("s");
+    if (verticalKeys.includes(event.key)) {
+        deleteSetValues(keysPressed, verticalKeys);
         movementY = 0;
     }
-    else if (event.key === "a" || event.key === "d") {
-        keysPressed.delete("a");
-        keysPressed.delete("d");
+    else if (horizontalKeys.includes(event.key)) {
+        deleteSetValues(keysPressed, horizontalKeys);
         movementX = 0;
     }
-    if (keysPressed.has("w") || keysPressed.has("a") || keysPressed.has("s") || keysPressed.has("d")) {
+    if (setHasValues(keysPressed, movementKeys)) {
         intervalId = setInterval(() => {
             xMove.value = parseInt(xMove.value || "0", 10) + movementX;
             yMove.value = parseInt(yMove.value || "0", 10) + movementY;
             draw();
-            // console.log("xMove.value, yMove.value:", xMove.value, yMove.value);
         }, 16);
     }
 }
